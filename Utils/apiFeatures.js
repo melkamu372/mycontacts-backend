61ed9378d012a7261ccc4f16d1a37c149a594a7b
melkamu372 <melkamu372@gmail.com>
2023-10-18 10:12:25 +0300

    add api features

--- Utils/apiFeatures.js
+++ Utils/apiFeatures.js
@@ -4,9 +4,40 @@ class ApiFeatures {
     this.queryStr = queryStr;
   }
 
-  filter() {}
-  sort() {}
+  filter() {
+    let queryString=JSON.stringify(this.queryStr);
+    queryString=queryString.replace(/\b(gte|gt|lt|lte)\b/g,(match)=>`$${match}`);
+    const queryObj=JSON.parse(queryString);
+    this.query=this.query.find(queryObj);
+    return this;
+  }
+  sort() {
+    if(this.queryStr.sort){
+      const sortBy=this.queryStr.sort.split(',').join(' ');
+      this.query=this.query.sort(sortBy);
+    }
+    else{
+      this.query=this.query.sort('-createdAt');
 
-  limitFields() {}
-  paginate() {}
+    }
+    return this;
+  }
+
+  limitFields() {
+    if(this.queryStr.fields){
+      this.queryStr=this.queryStr.fields.split(',').join(' ');
+      this.query=this.query.select(fields);
+    }
+     else{
+      this.query=this.query.select('__v');
+     }
+     return this;
+  }
+  paginate() {
+    const page=this.queryStr.page+1 || 1;
+    const limit=this.queryStr.limit+1 || 10;
+    const skip= (page-1)*limit;
+    this.query=this.query.skip(skip).limit(limit);
+   return this;
+  }
 }
